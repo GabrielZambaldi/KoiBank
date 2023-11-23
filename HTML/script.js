7d1b6cebf92c601744071465e24c6157f3d58fde
ollondo <danielamaralpereira2@gmail.com>
2023-11-22 22:04:57 -0300

    Testando o js e a comunicação com o backend

---
+++ HTML/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById('meuformulario');
+
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault();
+        console.log('Evento de envio acionado!');
+
+        // Obter dados do formulário
+        const dadosDoFormulario = {
+            primeiroNome: document.querySelector('input[name="primeiro nome"]').value,
+            ultimoNome: document.querySelector('input[name="último nome"]').value,
+            cpf: document.querySelector('input[name="cpf"]').value,
+            email: document.querySelector('input[name="email"]').value,
+            // Adicionar outros campos conforme necessário
+        };
+
+        // Enviar dados para o backend
+        enviarParaBackend(dadosDoFormulario);
+    });
+});
+
+function enviarParaBackend(dados) {
+    // URL do seu backend
+    const urlBackend = '/'; // Substitua pelo URL real do seu backend
+
+    // Fazer uma requisição POST usando fetch
+    fetch(urlBackend, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dados),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Resposta do backend:', data);
+
+        // Adicione lógica para lidar com a resposta do backend, se necessário
+    })
+    .catch(error => {
+        console.error('Erro ao enviar para o backend:', error);
+    });
+}
