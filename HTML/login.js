73a2b7db05278556f51f04290ca85a85f6e6455a
ollondo <danielamaralpereira2@gmail.com>
2023-11-26 00:31:33 -0300

    Bora bil

--- HTML/login.js
+++ HTML/login.js
@@ -10,25 +10,37 @@ document.addEventListener('DOMContentLoaded', function() {
         };
 
         fetch('http://127.0.0.1:5000/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(dadosDoFormulario),
-})
-
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dadosDoFormulario),
+        })
         .then(response => response.json())
         .then(data => {
             console.log(data);
 
             if ('mensagem' in data && data.mensagem === 'Login bem-sucedido!') {
+                const idUsuario = data.id_usuario;
+
+                // Exemplo de uma requisição para obter mensagens personalizadas
+                fetch(`http://127.0.0.1:5000/mensagens/${idUsuario}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        if ('mensagem_personalizada' in data) {
+                            alert(data.mensagem_personalizada);
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Erro ao obter mensagens personalizadas:', error);
+                    });
+
                 alert('Login realizado com sucesso!');
                 // Adicione lógica adicional para o sucesso do login, se necessário
                 window.location.href = './acesso/acesso.html';  // Redireciona para a próxima página
             } else if ('erro' in data) {
                 alert('Credenciais incorretas. Tente novamente.');
             } else {
-                // Trate qualquer outra condição ou resposta inesperada aqui
                 console.error('Resposta inesperada do servidor:', data);
             }
         })
