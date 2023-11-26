d24d98f5099ebe6d00ad69e705fab9b11eee4d63
ollondo <danielamaralpereira2@gmail.com>
2023-11-26 00:22:15 -0300

    Está funcionando o back end

--- HTML/login.js
+++ HTML/login.js
@@ -10,12 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
         };
 
         fetch('http://127.0.0.1:5000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(dadosDoFormulario),
-        })
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dadosDoFormulario),
+})
+
         .then(response => response.json())
         .then(data => {
             console.log(data);
